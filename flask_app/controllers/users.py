5176ac0b9f88cf89c4fdff43c9a6e7345d568d72
Jaden Sierra <JadenSierra31@gmail.com>
2022-01-23 19:59:11 -0800

    Initial_commit

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,52 @@
+from flask_app import app
+from flask import render_template,redirect,request,session,flash
+from flask_app.models.user import User
+
+
+@app.route('/')
+def home_page():
+    users = User.get_all()
+    return render_template("index.html", users = users)
+
+@app.route('/create')
+def create_user():
+    return render_template("create.html")
+
+@app.route('/create/user', methods=["POST"])
+def create_friend():
+    data = {
+        "fname": request.form['fname'],
+        "lname": request.form['lname'],
+        "email": request.form['email']
+    }
+    User.save(data)
+    return redirect('/')
+
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data = {
+        "id" : id
+    }
+    User.destroy(data)
+    return redirect("/")
+
+@app.route('/user/edit/<int:id>')
+def edit_user(id):
+    data = {
+        "id": id
+    }
+    user = User.get_one(data)
+    return render_template("edit.html", user = user)
+
+@app.route('/update/user', methods = ["POST"])
+def update_user():
+    User.update(request.form)
+    return redirect('/')
+
+@app.route("/user/show/<int:id>")
+def show(id):
+    data = {
+        'id': id
+    }
+    user = User.get_one(data)
+    return render_template("show.html", user = user)
